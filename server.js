9d4af37374571a01714ad4fcc51f5e1420702c65
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2025-05-31 17:38:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(cors());
 app.use(express.json({limit: '20mb'}));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
+app.get ("/", (req,res) => {
+    res.send('Vrundavan API is live!');
+})
+
 app.use('/public', express.static('public'));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use('/invoice', express.static(path.join(__dirname, 'public/invoice')));
