eaa32eaac1777b9a06e890c7a69789528694864b
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2025-06-04 17:25:52 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require ('dotenv').config();
 const mysql = require('mysql2');
 
-var con = mysql.createPool({
+var con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
