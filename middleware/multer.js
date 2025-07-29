f073a782ab8c898f6f6455edf55e3526d72efe6c
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2025-07-29 13:22:32 +0530

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -30,20 +30,25 @@
 // const upload = multer({ storage });
 
 // module.exports = upload;
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
-const { CloudinaryStorege } = require('multer-storage-cloudinary');
-const cloudinary = require('./cloudinary');
-const { param } = require('../routes/api_routes');
 
-const storege = new CloudinaryStorege({
-  cloudinary,
-  param: {
-    folder: 'products',
-    allowed_formates: ['jpg','png','jpeg','webp']
-  }
-})
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
-const upload = multer({storage});
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads',
+    allowed_formats: ['jpg', 'png'],
+  },
+});
+
+const upload = multer({ storage: storage });
 
 module.exports = upload;
 
