ac545bc7b0d426e221810e1210e904fa63b7d92c
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2025-08-02 11:25:56 +0530

    Update invoice.js

--- routes/invoice.js
+++ routes/invoice.js
@@ -9,25 +9,30 @@ const jsPDF = require("jspdf");
 // 📌 POST route to receive image, convert to PDF, store it, save DB, send WhatsApp
 router.post("/bills", async (req, res) => {
   try {
-    const {  filename, mobileNumber, orderDate, customerName, pdfBase64 } = req.body;
+    // const {  filename, mobileNumber, orderDate, customerName, pdfBase64 } = req.body;
 
-    console.log(filename, mobileNumber, orderDate, customerName);
+    // console.log(filename, mobileNumber, orderDate, customerName);
 
-    if ( !filename || !mobileNumber || !orderDate || !customerName || !pdfBase64) {
-      return res.status(400).json({ success: false, message: "Missing required fields." });
-    }
+    // if ( !filename || !mobileNumber || !orderDate || !customerName || !pdfBase64) {
+    //   return res.status(400).json({ success: false, message: "Missing required fields." });
+    // }
 
-    const filePath = path.join(__dirname, "../public/invoice", filename);
+    // const filePath = path.join(__dirname, "../public/invoice", filename);
 
-    // Ensure the directory exists
-    const dir = path.join(__dirname, "../public/invoice");
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true });
-    }
+    // // Ensure the directory exists
+    // const dir = path.join(__dirname, "../public/invoice");
+    // if (!fs.existsSync(dir)) {
+    //   fs.mkdirSync(dir, { recursive: true });
+    // }
 
-    //save PDF from base64
-    const buffer = Buffer.from(pdfBase64, "base64");
-    fs.writeFileSync(filePath, buffer);
+    // //save PDF from base64
+    // const buffer = Buffer.from(pdfBase64, "base64");
+    // fs.writeFileSync(filePath, buffer);
+
+      const {customerName, mobileNumber, orderDate} = req.body;
+    const {filename} = req.file;
+
+    console.log(res.body);
 
     // Insert into database
     const insert = "INSERT INTO `bills` (`cust_name`, `pdf`, `mobile`, `date`) VALUES (?, ?, ?, ?)";
